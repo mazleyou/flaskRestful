86a766a78fa6e58c3e7a32c2e7507fe89169dac7
mazleyou <mazle@nate.com>
2020-03-25 19:35:58 +0900

    200313

--- test.py
+++ test.py
@@ -3,16 +3,7 @@ from bs4 import BeautifulSoup
 
 driver = webdriver.Chrome('drivers/chromedriver')
 driver.implicitly_wait(3)
-driver.get('https://nid.naver.com/nidlogin.login')
-driver.find_element_by_name('id').send_keys('naver_id')
-driver.find_element_by_name('pw').send_keys('mypassword1234')
-driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
 
 # Naver 페이 들어가기
 driver.get('https://order.pay.naver.com/home')
-html = driver.page_source
-soup = BeautifulSoup(html, 'html.parser')
-notices = soup.select('div.p_inr > div.p_info > a > span')
-
-for n in notices:
-    print(n.text.strip())
+print("Page Title is : %s" %driver.title)
